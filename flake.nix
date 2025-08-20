dfd9e55160ef8548e6a34d5fdeb7bed5cb437cd7
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-21 01:21:08 +0900

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "zola themes";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python313
+            python313Packages.toml
+          ];
+        };
+      }
+    );
+}
